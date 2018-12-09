e594078981931c5e6af821d6aface499cd3e9ae8
Eduardo Márquez <eduardo20.3263@gmail.com>
2018-12-09 19:55:03 -0400

    Feat: Configuración para el entorno de pruebas automatizadas (PHPUnit)

--- config/database.php
+++ config/database.php
@@ -45,13 +45,6 @@ return [
     */
 
     'connections' => [
-
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-        ],
-
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -66,19 +59,19 @@ return [
             'engine' => null,
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
+        'mysql_tests' => [
+            'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE_TESTS', 'foro_styde_tests'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
             'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
+            'strict' => true,
+            'engine' => null,
         ],
-
     ],
 
     /*
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -7,13 +7,44 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class ExampleTest extends TestCase
 {
     /**
+     * El problema de este Trait es que si tenemos muchísimas migraciones, las
+     * pruebas van a ser demasiada lentas. Es por ello que es conveniente usar
+     * el otro Trait.
+     */
+    //use DatabaseMigrations;
+
+    /**
+     * El detalle de este Trait es que debemos correr las migraciones UNA SOLA
+     * VEZ de forma manual, ejecutando: php artisan migrate --database=mysql_tests
+     */
+    use DatabaseTransactions;
+
+    /**
      * A basic functional test example.
      *
      * @return void
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel');
+        $user = factory(\App\User::class)->create([
+            'name' => 'Eduardo Marquez',
+            'email' => 'eduardo20.3263@gmail.com'
+        ]);
+
+        $this->actingAs($user, 'api')
+             ->visit('api/user')
+             ->see('Eduardo Marquez')
+             ->see('eduardo20.3263@gmail.com');
+
+        /**
+         * Esto último resulta muy inteligente pero es importante que para el
+         * Testing no dependamos de valores aleatorios
+         */
+
+        //$user = factory(\App\User::class)->create();
+        //
+        //$this->actingAs($user, 'api')
+        //    ->visit('api/user')
+        //    ->see($user->name);
     }
 }
