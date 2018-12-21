a19c02e1ce418c9347c82225c8dc8c420d82a9b3
Eduardo Márquez <eduardo20.3263@gmail.com>
2018-12-21 15:15:13 +0000

    Feat: Verificar features ya desarrollados usando pruebas de regresión en Laravel

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,8 +7,17 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function show(Post $post)
+    public function show(Post $post, $slug)
     {
+        // Comentamos estas líneas ya que haremos uso de una nueva funcionalidad.
+        // Ver el test_post_url_with_wrong_slug_still_work()
+        //abort_unless($post->slug == $slug, 404);
+        //abort_if($post->slug != $slug, 404);
+
+        if ($post->slug != $slug) {
+            return redirect($post->url, 301);
+        }
+
         return view('posts.show', compact('post'));
     }
 }
--- tests/features/ShowPostTest.php
+++ tests/features/ShowPostTest.php
@@ -22,9 +22,69 @@ class ShowPostTest extends TestCase
         $user->posts()->save($post);
 
         // When
-        $this->visit(route('posts.show', $post))
+        $this->visit($post->url)
             ->seeInElement('h1', $post->title)
             ->see($post->content)
             ->see($user->name);
     }
+
+    function test_old_urls_are_redirected()
+    {
+        // Having
+        $user = $this->defaultUser([
+            'name' => 'Eduardo Marquez',
+        ]);
+
+        $post = factory(\App\Post::class)->make([
+            'title' => 'Old title',
+        ]);
+
+        $user->posts()->save($post);
+
+        $url = $post->url;
+
+        $post->update(['title' => 'New title']);
+        // When
+        $this->visit($url)
+            ->seePageIs($post->url);
+    }
+
+    /**
+     * Esta prueba la comentaremos ya que haremos uso de una nueva funcionalidad.
+     * Una que permita buscar las url viejas y las redireccione a las nuevas url.
+     */
+    //function test_post_url_with_wrong_slug_still_work()
+    //{
+    //    // Having
+    //    $user = $this->defaultUser([
+    //        'name' => 'Eduardo Marquez',
+    //    ]);
+    //
+    //    $post = factory(\App\Post::class)->make([
+    //        'title' => 'Old title',
+    //    ]);
+    //
+    //    $user->posts()->save($post);
+    //
+    //    $url = $post->url;
+    //
+    //    $post->update(['title' => 'New title']);
+    //
+    //    // When
+    //    //$this->visit($url)
+    //    //    ->assertResponseStatus(200)
+    //    //    //También podemos usar
+    //    //    //->assertResponseOk()
+    //    //    ->see('New title');
+    //
+    //    /**
+    //     * Esto anterior o debería funcionar porque un usuario puede ingresar un
+    //     * slug random y la página seguiría funcionando. Es por ello que debería
+    //     * retornar entonces es un error 404
+    //     */
+    //
+    //    // When
+    //    $this->get($url)
+    //        ->assertResponseStatus(404);
+    //}
 }
