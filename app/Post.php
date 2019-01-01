a1222ec69b82a5a33c1e6117ef41a29373e18940
Eduardo Márquez <eduardo20.3263@gmail.com>
2019-01-01 23:30:04 +0000

    Feat: Módulo de comentarios en Laravel 5.3 con TDD

--- app/Post.php
+++ app/Post.php
@@ -17,6 +17,11 @@ class Post extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
