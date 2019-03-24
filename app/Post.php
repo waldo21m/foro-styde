ea4d2957af059a53c35ccc1f715d6bec67275348
Eduardo Márquez <eduardo20.3263@gmail.com>
2019-03-24 21:42:47 +0000

    Add markAsAnswer method to the comment model

--- app/Post.php
+++ app/Post.php
@@ -12,6 +12,10 @@ class Post extends Model
         'content',
     ];
 
+    protected $casts = [
+        'pending' => 'boolean',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- database/migrations/2019_01_01_231303_create_comments_table.php
+++ database/migrations/2019_01_01_231303_create_comments_table.php
@@ -16,6 +16,7 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->mediumText('comment');
+            $table->boolean('answer')->default(false);
             $table->unsignedInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->unsignedInteger('post_id');
